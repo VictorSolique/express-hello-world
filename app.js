f5947b728a82a61618ef88b7db1a3483aeb2d1ae
Victor Solique <victordesouzasolique@gmail.com>
2024-12-03 09:42:11 -0400

    Add endpoint GET/tico

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.get('/meunome', (req, res) => {
   res.send("Meu nome é Victor de Souza Solique");
 });
 
+app.get('/tico', (req, res) => {
+  res.send("teco");
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
